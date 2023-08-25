90a11a19aa5eda80ae08dc511de89606684ef6b1
Deepak Raj <rk7763007@gmail.com>
2023-08-25 13:24:53 +0530

    Git Ignore file updated

---
+++ Controllers/InterviewController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechnorucsWalkInAPI.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InterviewController : Controller
+    {
+
+
+        [HttpGet]
+        [Route("GetAllInterviews")]
+        public dynamic GetAllInterviews()
+        {
+
+            return"All Inteviews";
+        }
+    }
+}
