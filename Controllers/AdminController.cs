2983be75b858228c7acff81a3047c0acaa3c7c01
vinoth <vinoth@technorucs.com>
2023-08-25 12:17:46 +0530

    Startup added

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -19,12 +19,14 @@ namespace TechnorucsWalkInAPI.Controllers
     public class AdminController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+        private readonly ClientContext _clientContext;
+        private readonly string _adminList;
 
-        public AdminController(IConfiguration configuration)
+        public AdminController(IConfiguration configuration, ClientContext clientContext)
         {
-            _configuration = configuration;           
-           
-            
+            _configuration = configuration;
+            _clientContext = clientContext;
+            _adminList = configuration["adminList"];
         }
         #region AdminLogin
         [AllowAnonymous]
@@ -32,64 +34,57 @@ namespace TechnorucsWalkInAPI.Controllers
         [Route("AdminLogin")]
         public dynamic Login([FromBody] AdminLoginModel model)
         {
-            string siteUrl = _configuration["siteurl"];
-            string adminList = _configuration["adminList"];
-            string appId = _configuration["appId"];
-            string appSecret = _configuration["appSecret"];
             try
             {
-                using (ClientContext clientContext = new PnP.Framework.AuthenticationManager().GetACSAppOnlyContext(siteUrl, appId, appSecret))
+                List targetList = _clientContext.Web.Lists.GetByTitle(_adminList);
+                CamlQuery query = new CamlQuery();
+                query.ViewXml = $@"<View><Query><Where><Eq><FieldRef Name='Email' /><Value Type='Text'>{model.Email}</Value></Eq></Where></Query></View>";
+                ListItemCollection items = targetList.GetItems(query);
+                _clientContext.Load(items);
+                _clientContext.ExecuteQuery();
+                if (items.Count == 1)
                 {
-                    List targetList = clientContext.Web.Lists.GetByTitle(adminList);
-                    CamlQuery query = new CamlQuery();
-                    query.ViewXml = $@"<View><Query><Where><Eq><FieldRef Name='Email' /><Value Type='Text'>{model.Email}</Value></Eq></Where></Query></View>";
-                    ListItemCollection items = targetList.GetItems(query);
-                    clientContext.Load(items);
-                    clientContext.ExecuteQuery();
-                    if (items.Count == 1)
+                    ListItem item = items[0];
+                    string password = item["Password"].ToString();
+                    string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+                    bool isMatch = BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+                    if (isMatch)
                     {
-                        ListItem item = items[0];
-                        string password = item["Password"].ToString();
-                        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
-                        bool isMatch = BCrypt.Net.BCrypt.Verify(password, hashedPassword);
-                        if (isMatch)
+                        AdminTokenModel tokenModel = new AdminTokenModel();
+                        tokenModel.Name = item["Title"].ToString();
+                        tokenModel.Role = "Admin";
+                        var Token = GenerateToken(tokenModel);
+                        bool hasAccess = Boolean.Parse(item["IsApproved"].ToString());
+                        if (hasAccess)
                         {
-                            AdminTokenModel tokenModel= new AdminTokenModel();
-                            tokenModel.Name = item["Title"].ToString();
-                            tokenModel.Role = "Admin";
-                            var Token = GenerateToken(tokenModel);
-                            bool hasAccess = Boolean.Parse(item["IsApproved"].ToString());
-                            if (hasAccess)
+                            return Ok(new
                             {
-                                return Ok(new
-                                {
-                                    status = "Login success",
-                                    id = item["ID"].ToString(),
-                                    Name = item["Title"].ToString(),
-                                    Email = item["Email"].ToString(),
-                                    IsApproved = item["IsApproved"].ToString(),
-                                    IsDeleted = item["IsDeleted"].ToString(),
-                                    token = Token
-                                });
-                            }
-                            else
-                            {
-                                return BadRequest(new
-                                {
-                                    status = "You don't have access",
-                                });
-                            }
-                           
+                                status = "Login success",
+                                id = item["ID"].ToString(),
+                                Name = item["Title"].ToString(),
+                                Email = item["Email"].ToString(),
+                                IsApproved = item["IsApproved"].ToString(),
+                                IsDeleted = item["IsDeleted"].ToString(),
+                                token = Token
+                            });
                         }
                         else
                         {
-                            return BadRequest("Invalid Password");
+                            return BadRequest(new
+                            {
+                                status = "You don't have access",
+                            });
                         }
+
                     }
                     else
                     {
-                        return BadRequest( "User not found");
+                        return BadRequest("Invalid Password");
                     }
+                }
+                else
+                {
+                    return BadRequest("User not found");
                 }
             }
             catch (Exception ex)
@@ -111,59 +106,51 @@ namespace TechnorucsWalkInAPI.Controllers
             {
                 return "Please give   all the required details";
             }
-            string siteUrl = _configuration["siteurl"];
-            string adminList = _configuration["adminList"];
-            string appId = _configuration["appId"];
-            string appSecret = _configuration["appSecret"];
 
             try
             {
-                using (ClientContext clientContext = new PnP.Framework.AuthenticationManager().GetACSAppOnlyContext(siteUrl, appId, appSecret))
+                List targetList = _clientContext.Web.Lists.GetByTitle(_adminList);
+                CamlQuery query = new CamlQuery();
+                query.ViewXml = $@"<View><Query><Where><Eq><FieldRef Name='Email' /><Value Type='Text'>{model.Email}</Value></Eq></Where></Query></View>";
+                ListItemCollection items = targetList.GetItems(query);
+                _clientContext.Load(items);
+                _clientContext.ExecuteQuery();
+                if (items.Count == 0)
                 {
-                    List targetList = clientContext.Web.Lists.GetByTitle(adminList);
-                    CamlQuery query = new CamlQuery();
-                    query.ViewXml = $@"<View><Query><Where><Eq><FieldRef Name='Email' /><Value Type='Text'>{model.Email}</Value></Eq></Where></Query></View>";
-                    ListItemCollection items = targetList.GetItems(query);
-                    clientContext.Load(items);
-                    clientContext.ExecuteQuery();
-                    if (items.Count == 0)
+                    List list = _clientContext.Web.Lists.GetByTitle(_adminList);
+                    ListItemCreationInformation listItemCreationInformation = new ListItemCreationInformation();
+                    ListItem listItem = list.AddItem(listItemCreationInformation);
+                    listItem["Title"] = model.Name;
+                    listItem["Email"] = model.Email;
+                    string hashedPassword = BCrypt.Net.BCrypt.HashPassword(model.Password);
+                    listItem["Password"] = hashedPassword;
+                    listItem.Update();
+                    _clientContext.ExecuteQuery();
+                    return new
                     {
-                        List list = clientContext.Web.Lists.GetByTitle(adminList);
-                        ListItemCreationInformation listItemCreationInformation = new ListItemCreationInformation();
-                        ListItem listItem = list.AddItem(listItemCreationInformation);
-                        listItem["Title"] = model.Name;
-                        listItem["Email"] = model.Email;
-                        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(model.Password);
-                        listItem["Password"] = hashedPassword;
-                        listItem.Update();
-                        clientContext.ExecuteQuery();
-                        return new
-                        {
-                            status = "Registered successfully",
-                            id = listItem["ID"].ToString(),
-                            Name = listItem["Title"].ToString(),
-                            Email = listItem["Email"].ToString(),
-                            IsApproved = listItem["IsApproved"].ToString(),
-                            IsDeleted = listItem["IsDeleted"].ToString()
-                        };
-                    }
-                    else
+                        status = "Registered successfully",
+                        id = listItem["ID"].ToString(),
+                        Name = listItem["Title"].ToString(),
+                        Email = listItem["Email"].ToString(),
+                        IsApproved = listItem["IsApproved"].ToString(),
+                        IsDeleted = listItem["IsDeleted"].ToString()
+                    };
+                }
+                else
+                {
+                    return new
                     {
-                        return new
-                        {
-                            status = "User Already Exists",
-                            id = items[0]["ID"].ToString(),
-                            Name = items[0]["Title"].ToString(),
-                            Email = items[0]["Email"].ToString(),
-                            IsApproved = items[0]["IsApproved"].ToString(),
-                            IsDeleted = items[0]["IsDeleted"].ToString()
-                        };
-                    }
+                        status = "User Already Exists",
+                        id = items[0]["ID"].ToString(),
+                        Name = items[0]["Title"].ToString(),
+                        Email = items[0]["Email"].ToString(),
+                        IsApproved = items[0]["IsApproved"].ToString(),
+                        IsDeleted = items[0]["IsDeleted"].ToString()
+                    };
                 }
             }
             catch (Exception ex)
             {
-
                 throw new Exception(ex.Message);
             }
 
@@ -177,36 +164,27 @@ namespace TechnorucsWalkInAPI.Controllers
         [Route("ApproveAdmin")]
         public dynamic ApproveAdmin([FromBody] AdminApprovalModel model)
         {
-            string siteUrl = _configuration["siteurl"];
-            string adminList = _configuration["adminList"];
-            string appId = _configuration["appId"];
-            string appSecret = _configuration["appSecret"];
 
             try
             {
-                using (ClientContext clientContext = new PnP.Framework.AuthenticationManager().GetACSAppOnlyContext(siteUrl, appId, appSecret))
+                List targetList = _clientContext.Web.Lists.GetByTitle(_adminList);
+                ListItem listItem = targetList.GetItemById(model.id);
+                listItem["IsDeleted"] = model.isApproved;
+                listItem.Update();
+                _clientContext.ExecuteQuery();
+                if (model.isApproved)
                 {
-
-                    List targetList = clientContext.Web.Lists.GetByTitle(adminList);
-                   ListItem listItem=targetList.GetItemById(model.id);
-                    listItem["IsDeleted"] =model.isApproved ;
-                    listItem.Update();
-                    clientContext.ExecuteQuery();
-                    if(model.isApproved)
+                    return new
                     {
-                        return new
-                        {
-                            status = "The admin is Deleted"
-                        };
-                    }
-                    else
+                        status = "The admin is Deleted"
+                    };
+                }
+                else
+                {
+                    return new
                     {
-                        return new
-                        {
-                            status = "The admin is Deleted"
-                        };
-                    }
-                    
+                        status = "The admin is Deleted"
+                    };
                 }
             }
             catch (Exception ex)
@@ -223,36 +201,26 @@ namespace TechnorucsWalkInAPI.Controllers
         [Route("DeleteAdmin")]
         public dynamic DeleteAdmin([FromBody] AdminDeleteModel model)
         {
-            string siteUrl = _configuration["siteurl"];
-            string adminList = _configuration["adminList"];
-            string appId = _configuration["appId"];
-            string appSecret = _configuration["appSecret"];
-
             try
             {
-                using (ClientContext clientContext = new PnP.Framework.AuthenticationManager().GetACSAppOnlyContext(siteUrl, appId, appSecret))
+                List targetList = _clientContext.Web.Lists.GetByTitle(_adminList);
+                ListItem listItem = targetList.GetItemById(model.id);
+                listItem["IsApproved"] = model.IsDeleted;
+                listItem.Update();
+                _clientContext.ExecuteQuery();
+                if (model.IsDeleted)
                 {
-
-                    List targetList = clientContext.Web.Lists.GetByTitle(adminList);
-                    ListItem listItem = targetList.GetItemById(model.id);
-                    listItem["IsApproved"] = model.IsDeleted;
-                    listItem.Update();
-                    clientContext.ExecuteQuery();
-                    if (model.IsDeleted)
+                    return new
                     {
-                        return new
-                        {
-                            status = "The admin is Approved"
-                        };
-                    }
-                    else
+                        status = "The admin is Approved"
+                    };
+                }
+                else
+                {
+                    return new
                     {
-                        return new
-                        {
-                            status = "The admin is UnApproved"
-                        };
-                    }
-
+                        status = "The admin is UnApproved"
+                    };
                 }
             }
             catch (Exception ex)
---
+++ Startup.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Localization; 
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.SharePoint.Client;
+using PnP.Framework.Provisioning.Model;
+
+namespace TechnorucsWalkInAPI
+{
+    public class Startup
+    {
+        public IConfiguration configRoot
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            configRoot = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options => {
+                options.RequireHttpsMetadata = true;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidIssuer = configRoot["Jwt:Issuer"],
+                    ValidAudience = configRoot["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configRoot["Jwt:Key"]))
+                };
+            });
+
+            services.AddTransient<ClientContext>(_ => new PnP.Framework.AuthenticationManager().GetACSAppOnlyContext(configRoot["siteurl"], configRoot["appId"], configRoot["appSecret"]));
+
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Description = "Please enter JWT with Bearer into field",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Technorucs WalkIN API");
+            });
+
+            app.Run();
+
+        }
+    }
+}
